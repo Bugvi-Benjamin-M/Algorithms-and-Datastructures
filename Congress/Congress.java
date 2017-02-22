4e8c96bc171539a3c7ecf1d7c1c498754c77f3b6
Andreas Blanke <blan@itu.dk>
2017-02-22 16:00:30 +0100

    Congress first iteration

--- Congress/Congress.java
+++ Congress/Congress.java
@@ -1,5 +1,15 @@
+import edu.princeton.cs.algs4.*;
+
+/* CLIENT */
 class Congress{
     public static void main(String[] args) {
-        
+        int numberOfStates = StdIn.readInt();
+        int numberOfSeats = StdIn.readInt();
+        HHQueue pQueue = new HHQueue(numberOfStates);
+        for (int i = 0; i < numberOfStates; i++) {
+            String stateName = StdIn.readString();
+            long statePop = StdIn.readLong();
+            pQueue.insert(stateName,statePop);
+        }
     }
 }
--- Congress/HHQueue.java
+++ Congress/HHQueue.java
@@ -1,3 +1,71 @@
-class HHQueue{
-    //
+/**
+ * Priory Queue using the Huntington-Hill method
+ */
+public class HHQueue{
+
+    private State[] collection;
+    private long size;
+    private int capacity;
+
+    class State {
+        double priority;
+        int allocatedSeats = 1;
+        long population;
+        String name;
+
+        void calculatePriority() {
+            priority = (population /
+                    Math.sqrt(
+                            allocatedSeats * (allocatedSeats + 1)));
+        }
+    }
+
+    public HHQueue(int capacity) {
+        collection = new State[capacity+1];
+        this.capacity = capacity;
+        this.size = 0;
+    }
+
+    public void insert(String stateName, long population) {
+        State newState = new State();
+        newState.name = stateName;
+        newState.population = population;
+        newState.calculatePriority();
+        collection[++size] = newState;
+        swim(size);
+    }
+
+    private void swim(int position) {
+        while(position > 1 && less(position/2,position)) {
+            exchange(position/2,position);
+            position = position/2;
+        }
+    }
+
+    private void sink(int position) {
+        while (2*position <= size) {
+            int counter = 2*position;
+            if (counter < size && less(counter,counter+1)) {
+                counter++;
+            }
+            if(!less(position,counter)) {
+                break;
+            } else {
+                exchange(position,counter);
+                position = counter;
+            }
+        }
+    }
+
+    private boolean less(int positionOfParent, int positionOfChild) {
+        return collection[positionOfParent].priority
+                < collection[positionOfChild].priority;
+    }
+
+    private void exchange(int positionOfParent, int positionOfChild) {
+        State parent = collection[positionOfParent];
+        State child = collection[positionOfChild];
+        collection[positionOfParent] = child;
+        collection[positionOfChild] = parent;
+    }
 }
