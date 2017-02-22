d49a9688e8492fb2e2a8aa43b54a30785c703916
Niclas Hedam <niclas@frax.dk>
2017-02-22 17:11:05 +0100

    Format :: Google

--- Congress/Congress.java
+++ Congress/Congress.java
@@ -1,20 +1,19 @@
 import edu.princeton.cs.algs4.*;
 
 /* CLIENT */
-class Congress{
-    public static void main(String[] args) {
-        int numberOfStates = Integer.parseInt(StdIn.readLine());
-        int numberOfSeats = Integer.parseInt(StdIn.readLine());
-        HHQueue pQueue = new HHQueue(numberOfStates);
-        for (int i = 0; i < numberOfStates; i++) {
-            String stateName = StdIn.readLine();
-            int statePop = Integer.parseInt(StdIn.readLine());
-            pQueue.insert(stateName,statePop);
-        }
-        for(int i = numberOfStates ; i < numberOfSeats ; i++){
-            pQueue.addSeat();
-        }
-        pQueue.printContents();
-
+class Congress {
+  public static void main(String[] args) {
+    int numberOfStates = Integer.parseInt(StdIn.readLine());
+    int numberOfSeats = Integer.parseInt(StdIn.readLine());
+    HHQueue pQueue = new HHQueue(numberOfStates);
+    for (int i = 0; i < numberOfStates; i++) {
+      String stateName = StdIn.readLine();
+      int statePop = Integer.parseInt(StdIn.readLine());
+      pQueue.insert(stateName, statePop);
     }
+    for (int i = numberOfStates; i < numberOfSeats; i++) {
+      pQueue.addSeat();
+    }
+    pQueue.printContents();
+  }
 }
--- Congress/HHQueue.java
+++ Congress/HHQueue.java
@@ -2,80 +2,77 @@ import edu.princeton.cs.algs4.*;
 /**
  * Priory Queue using the Huntington-Hill method
  */
-public class HHQueue{
+public class HHQueue {
+  private State[] collection;
+  private int size;
 
-    private State[] collection;
-    private int size;
+  class State {
+    double priority;
+    int allocatedSeats = 1;
+    int population;
+    String name;
 
-    class State {
-        double priority;
-        int allocatedSeats = 1;
-        int population;
-        String name;
+    void calculatePriority() {
+      priority = (population / Math.sqrt(allocatedSeats * (allocatedSeats + 1)));
+    }
+  }
 
-        void calculatePriority() {
-            priority = (population /
-                    Math.sqrt(allocatedSeats * (allocatedSeats + 1)));
-        }
-    }
+  public HHQueue(int capacity) {
+    collection = new State[capacity + 1];
+    this.size = 0;
+  }
 
-    public HHQueue(int capacity) {
-        collection = new State[capacity+1];
-        this.size = 0;
-    }
+  public void insert(String stateName, int population) {
+    State newState = new State();
+    newState.name = stateName;
+    newState.population = population;
+    newState.calculatePriority();
+    collection[++size] = newState;
+    swim(size);
+  }
 
-    public void insert(String stateName, int population) {
-        State newState = new State();
-        newState.name = stateName;
-        newState.population = population;
-        newState.calculatePriority();
-        collection[++size] = newState;
-        swim(size);
+  private void swim(int position) {
+    while (position > 1 && less(position / 2, position)) {
+      exchange(position / 2, position);
+      position = position / 2;
     }
+  }
 
-    private void swim(int position) {
-        while(position > 1 && less(position/2,position)) {
-            exchange(position/2,position);
-            position = position/2;
-        }
+  private void sink(int position) {
+    while (2 * position <= size) {
+      int counter = 2 * position;
+      if (counter < size && less(counter, counter + 1)) {
+        counter++;
+      }
+      if (!less(position, counter)) {
+        break;
+      } else {
+        exchange(position, counter);
+        position = counter;
+      }
     }
+  }
 
-    private void sink(int position) {
-        while (2*position <= size) {
-            int counter = 2*position;
-            if (counter < size && less(counter,counter+1)) {
-                counter++;
-            }
-            if(!less(position,counter)) {
-                break;
-            } else {
-                exchange(position,counter);
-                position = counter;
-            }
-        }
-    }
+  private boolean less(int positionOfParent, int positionOfChild) {
+    return collection[positionOfParent].priority < collection[positionOfChild].priority;
+  }
 
-    private boolean less(int positionOfParent, int positionOfChild) {
-        return collection[positionOfParent].priority
-                < collection[positionOfChild].priority;
-    }
+  private void exchange(int positionOfParent, int positionOfChild) {
+    State parent = collection[positionOfParent];
+    State child = collection[positionOfChild];
+    collection[positionOfParent] = child;
+    collection[positionOfChild] = parent;
+  }
 
-    private void exchange(int positionOfParent, int positionOfChild) {
-        State parent = collection[positionOfParent];
-        State child = collection[positionOfChild];
-        collection[positionOfParent] = child;
-        collection[positionOfChild] = parent;
+  public void addSeat() {
+    collection[1].allocatedSeats++;
+    collection[1].calculatePriority();
+    sink(1);
+  }
+
+  public void printContents() {
+    for (int i = 1; i < collection.length; i++) {
+      StdOut.println(collection[i].name + " " + collection[i].allocatedSeats);
     }
-    
-    public void addSeat(){
-        collection[1].allocatedSeats++;
-        collection[1].calculatePriority();
-        sink(1);
-    }
-    
-    public void printContents(){
-        for(int i = 1 ; i < collection.length ; i++){
-            StdOut.println(collection[i].name + " " + collection[i].allocatedSeats);
-        }
-    }
+  }
 }
