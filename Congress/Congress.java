0d67f85068f2d810d8c24fb0978220e2ced9ae9e
Andreas Blanke <blan@itu.dk>
2017-02-25 12:34:28 +0100

    Update Congress.java
    Added javadoc for Client

--- Congress/Congress.java
+++ Congress/Congress.java
@@ -1,7 +1,24 @@
 import edu.princeton.cs.algs4.*;
 
-/* CLIENT */
+/** 
+* Client class for using the HHQueue data structure.
+* Note that this class only works with a specific format of text files.
+* 
+* The required format of input files is as follows:
+*   number of states (integer, ex. 50)
+*   number of seats (integer, ex. 435)
+*   state name (string, ex. Wisconsin)
+*   population of state (integer, ex. 1000000)
+*   ad libitum (of states followed by their population)...
+* 
+* @author Andreas Blanke
+* @author Búgvi Magnussen
+* @author Jakob Mollerup
+* @author Niclas Hedam
+* @version 1.0
+*/
 class Congress {
+  
   public static void main(String[] args) {
     int numberOfStates = Integer.parseInt(StdIn.readLine());
     int numberOfSeats = Integer.parseInt(StdIn.readLine());
