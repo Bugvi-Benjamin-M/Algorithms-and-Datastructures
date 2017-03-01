2ce13bb117381258af65793f3c36592ae93604b1
Búgvi Magnussen <bugvimagnussen@gmail.com>
2017-03-01 21:04:49 +0100

    Javadoc and comments
    Wrote some Javadoc and comments for the FancyRunSort class

--- Runsort/FancyRunSort.java
+++ Runsort/FancyRunSort.java
@@ -37,6 +37,7 @@ public class FancyRunSort {
                         } else {
                                 low = 0;
                                 numberOfRounds++;
+                                StdStats.plotBars(a);
                         }
                 }
         }
