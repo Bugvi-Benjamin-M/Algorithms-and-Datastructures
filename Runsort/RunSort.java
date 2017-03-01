252500927281afc3fa1dd53dbcd2aa39360032ea
Búgvi Magnussen <bugvimagnussen@gmail.com>
2017-03-01 17:12:06 +0100

    Jeg kan lide kage
    Blanke har kage.

--- Runsort/RunSort.java
+++ Runsort/RunSort.java
@@ -1,9 +1,30 @@
 class RunSort
 {
   private static Comparable[] aux; // auxiliary array for merges
-  // See page 271 for merge() code.
+
   public static void sort(Comparable[] a)
-  { // Do lg N passes of pairwise merges.
+  { 
+      aux = new Comparable[a.length];
+      int low = 0;
+      int high = 0;
+      int mid = 0; 
+      
+      while(true) {
+          while(low < a.length-1) {
+              mid = findPointer(a, low);
+              if(mid == a.length-1) {
+                  break;
+              }
+              high = findPointer(a, mid + 1);
+              merge(a, low, mid, high);
+              low = high + 1;
+          } 
+          if(isSorted(a)) {
+              break;
+          } else {
+              low = 0;
+          }
+      }
   }
 
   public static void merge(Comparable[] a, int lo, int mid, int hi)
@@ -11,6 +32,7 @@ class RunSort
     int i = lo, j = mid + 1;
     for (int k = lo; k <= hi; k++) // Copy a[lo..hi] to aux[lo..hi].
       aux[k] = a[k];
+    
     for (int k = lo; k <= hi; k++) // Merge back to a[lo..hi].
       if (i > mid)
         a[k] = aux[j++];
@@ -34,4 +56,18 @@ class RunSort
         return false;
     return true;
   }
+  
+  private static int findPointer(Comparable[] a, int start) {
+      while(start < a.length) {
+          if(start == a.length-1) {
+              break;
+              
+              } else if(less(a[start], a[start+1])) {
+                  break;
+              }
+          start++;
+      }
+      return start;
+  }
 }
+
