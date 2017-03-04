38d6d73f77eb3913cde03a3de86724c51bb44a9b
Búgvi Magnussen <bugvimagnussen@gmail.com>
2017-03-04 17:41:20 +0100

    Added Javadoc, comments, and implemented visual trace extension

--- Runsort/RunSort.java
+++ Runsort/RunSort.java
@@ -1,17 +1,43 @@
 import edu.princeton.cs.algs4.*;
 
+/**
+ * This class implements a runsort algorithm, using several standard bottom up merge sort
+ * methods. However, the sort method is behaves differently and utlises natural
+ * orders that might already exist in the given array. This is know as natural merge sort. 
+ * @author Andreas Blanke
+ * @author Jakob Mollerup
+ * @author Búgvi Magnussen
+ * @author Niclas Hedam
+ * @version 1.0
+ */
+
 class RunSort {
-        private static Comparable[] aux; // auxiliary array for merges
-        private static int numberOfOperationsInThisProgramLOL;
-        private static int numberOfRunsInThisProgramLOLBlanke;
+        // auxiliary array for merges
+        private static Comparable[] aux; 
+        
+        // Number of operations used to sort input
+        private static int numberOfOperations;
+        
+        // Number of rounds used to sort input
+        private static int numberOfRuns;
+        
+        /**
+         * The natural merge sort sorting method. Identifies subarrays that are 
+         * already sorted in the given array. 
+         * @param A Comparable array to be sorted. 
+         */
         public static void sort(Comparable[] a) {
+            
+            //Initialise values
                 aux = new Comparable[a.length];
                 int low = 0;
                 int high = 0;
                 int mid = 0;
-                numberOfOperationsInThisProgramLOL = 0;
-                numberOfRunsInThisProgramLOLBlanke = 0;
-
+                numberOfOperations = 0;
+                numberOfRuns = 0;
+                
+                //Initiate sort
+                //Identify suquences, i.e. subarrays. 
                 while (true) {
                         while (low < a.length - 1) {
 
@@ -24,25 +50,35 @@ class RunSort {
                                 merge(a, low, mid, high);
 
                                 low = high + 1;
-                                numberOfRunsInThisProgramLOLBlanke++;
+                                numberOfRuns++;
                         }
                         if (isSorted(a)) {
                                 break;
                         } else {
                                 low = 0;
-                                numberOfOperationsInThisProgramLOL++;
+                                numberOfOperations++;
                         }
                 }
         }
 
+        /**
+         * Merge two sorted subarrays. Positions given as parameters to identify
+         * where the subarrays are positioned in the array.
+         * @param the array containing the subarrays to be sorted and merged
+         * @param start of first subarray
+         * @param end of first subarray
+         * @param end of second subarray
+         * 
+         */
         public static void
         merge(Comparable[] a, int lo, int mid,
-              int hi) { // Merge a[lo..mid] with a[mid+1..hi].
+              int hi) { 
                 int i = lo, j = mid + 1;
-                for (int k = lo; k <= hi; k++) // Copy a[lo..hi] to aux[lo..hi].
+                //Copy to auxiliary array
+                for (int k = lo; k <= hi; k++) 
                         aux[k] = a[k];
-
-                for (int k = lo; k <= hi; k++) // Merge back to a[lo..hi].
+                //sort and merge back to original array in sorted order.
+                for (int k = lo; k <= hi; k++) 
                         if (i > mid)
                                 a[k] = aux[j++];
                         else if (j > hi)
@@ -53,10 +89,22 @@ class RunSort {
                                 a[k] = aux[i++];
         }
 
+        /**
+         * Standard less method. Compares two elements. 
+         * @param first element to be compared
+         * @param second element to be compared
+         * @return true if the first element is less than the second, false otherwise. 
+         */
+        @SuppressWarnings("unchecked")
         private static boolean less(Comparable v, Comparable w) {
                 return v.compareTo(w) < 0;
         }
-
+        
+        /**
+         * Checks whether the given array is sorted 
+         * @param the array to check
+         * @return true if the array is sorted in increasing order, otherwise false. 
+         */
         public static boolean isSorted(
             Comparable[] a) { // Test whether the array entries are in order.
                 for (int i = 1; i < a.length; i++)
@@ -64,7 +112,14 @@ class RunSort {
                                 return false;
                 return true;
         }
-
+        
+        /**
+         * private method used by the sort algorithm to identify natural sequences and 
+         * determine subarrays.
+         * @param the array to be analysed. 
+         * @param the start position of the analysis
+         * @return the end position of the analysis
+         */
         private static int findPointer(Comparable[] a, int start) {
                 while (start < a.length) {
                         if (start == a.length - 1) {
@@ -77,7 +132,12 @@ class RunSort {
                 }
                 return start;
         }
-
+        
+        /**
+         * Prints the contents of a given array. 
+         * Prints information about the steps taken by the program
+         * @param the array to print. 
+         */
         private static void show(Comparable[] a) {
                 // Print the array, on a single line.
                 for (int i = 0; i < a.length; i++)
@@ -85,13 +145,17 @@ class RunSort {
                 StdOut.println();
         }
 
+        
+         /**
+         * This method executes the program and acts as client. 
+         */
         public static void main(String[] args) {
                 // Read strings from standard input, sort them, and print.
                 String[] a = StdIn.readAllStrings();
                 sort(a);
                 assert isSorted(a);
                 show(a);
-                StdOut.println(numberOfOperationsInThisProgramLOL);
-                StdOut.println(numberOfRunsInThisProgramLOLBlanke);
+                StdOut.println(numberOfOperations);
+                StdOut.println(numberOfRuns);
         }
 }
