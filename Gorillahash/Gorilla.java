5f81da8b47c5ae4831eb5801f7fb42c96f653d5c
Andreas Blanke <support@blanke.one>
2017-03-29 19:22:21 +0200

    final

--- Gorillahash/Gorilla.java
+++ Gorillahash/Gorilla.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Gorilla{
 
-    int dimension = 10000;  //d
+    int dimension = 100000000;  //d
     static int substringLength = 20;  //k
     String[] keys;
     int[] values;
@@ -34,34 +34,43 @@ public class Gorilla{
 
         while(keys[keyHashCode] != null){
             if(key.equals(keys[keyHashCode])){
-                values[keyHashCode]++;
+                values[keyHashCode] += 1;
                 break;
             }
             keyHashCode++;
             if(keyHashCode == keys.length) keyHashCode = 0;
         }
+
         if(keys[keyHashCode] == null){
-            keys[keyHashCode] = key;
-            values[keyHashCode] = 1;
+          keys[keyHashCode] = key;
+          values[keyHashCode] = 1;
         }
+
     }
 
     public int hashCode(String s){
-        return (s.hashCode() & 0x7fffffff) % dimension;
+        int hashcode = s.hashCode();
+        return (hashcode & 0x7fffffff) % dimension;
     }
 
     public void addSubstrings(){
-        int counter = 0;
-        StdOut.println("Size: "+proteinCode.length()+" divided by "+substringLength);
-        for(int i = 0 ; i <= proteinCode.length() - substringLength ; i++){
+        int counter = 0; int n = proteinCode.length();
+        String proteinCode = this.proteinCode.toString();
+        this.proteinCode = null;
+        // StdOut.println("Size: "+n+" divided by "+substringLength);
+        for(int i = 0 ; i <= n - substringLength; i++){
             put(proteinCode.substring(i, i + substringLength));
+            // proteinCode.delete(i, i + substringLength);
             counter++;
-            if (counter % 100 == 0) StdOut.println("... substrings: "+counter);
+            // if (counter % 1000 == 0)
+              // StdOut.println("count: "+counter+" i:"+i);
         }
+        proteinCode = null; n = 0; counter = 0;
+        // StdOut.println("Adding terminated...");
     }
 
     public static double vectorLength(int[] coordinates){
-        int sum = 0;
+        long sum = 0;
         for(int i : coordinates){
             sum += i * i;
         }
@@ -70,12 +79,12 @@ public class Gorilla{
 
     public static double vectorAngle(int[] a, int[] b){
         double angle;
-        angle = dotProduct(a, b) / (vectorLength(a) * vectorLength(b));
+        angle = (double) (dotProduct(a, b)) / (vectorLength(a) * vectorLength(b));
         return angle;
     }
 
-    public static int dotProduct(int[] a, int[] b){
-        int sum = 0;
+    public static long dotProduct(int[] a, int[] b){
+        long sum = 0;
 
         for(int i = 0 ; i < a.length ; i++){
             sum += a[i]*b[i];
@@ -86,7 +95,7 @@ public class Gorilla{
 
 
     public static void main(String[] args){
-        ArrayList<Gorilla> list = new ArrayList<Gorilla>();
+        ArrayList<Gorilla> list = new ArrayList<>();
 
         String input, name = "ignore";
         int nCodes = 0;
@@ -112,26 +121,37 @@ public class Gorilla{
                 gorilla.appendProteinCode(
                         input.substring(0,indexOfName));
                 nCodes++;
+                gorilla.addSubstrings();
                 name = input.substring(indexOfName + 1, input.length());
                 gorilla = new Gorilla(name);
                 list.add(gorilla);
             } // ignore if both is there (impossible)
-            if(nCodes % 1000 == 0) StdOut.println(""+name+":"+nCodes);
+            // if(nCodes % 1000 == 0) StdOut.println(""+name+":"+nCodes);
         }
 
-        StdOut.println("Finding and adding substrings...");
-        for(Gorilla g : list){
-            g.addSubstrings();
-            StdOut.println("Added substrings to: "+g.name);
-        }
+        gorilla.addSubstrings();
 
+        int[][] results = new int[list.size()][list.size()];
         StdOut.println("Comparing gorillas...");
         for (int j = 0; j < list.size(); j++) {
             int[] compare = list.get(j).getValues();
             for (int i = 0; i < list.size(); i++) {
-                StdOut.println(list.get(i).getName());
-                StdOut.println("" + vectorAngle(compare, list.get(i).getValues()));
+                double result = vectorAngle(compare, list.get(i).getValues());
+                results[j][i] = (int) (Math.floor(result * 100));
             }
         }
+
+      for (int i = 0; i < list.size(); i++) {
+        StdOut.print(list.get(i).name+" ");
+      }
+      StdOut.println();
+
+      for (int i = 0; i < results.length; i++) {
+        StdOut.print(list.get(i).name+":");
+        for (int j = 0; j < results[i].length; j++) {
+          StdOut.print(" "+results[i][j]+"%");
+        }
+        StdOut.println();
+      }
     }
 }
