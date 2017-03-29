2d36f79d522af6f16a0bd6d3119c730f3454bab0
Andreas Blanke <blan@itu.dk>
2017-03-29 14:31:31 +0200

    y u no work

--- Gorillahash/Gorilla.java
+++ Gorillahash/Gorilla.java
@@ -9,30 +9,29 @@ public class Gorilla{
     int[] values;
     String name;
     StringBuilder proteinCode;
-    
+
     public Gorilla(String name){
         proteinCode = new StringBuilder();
         keys = new String[dimension];
         values = new int[dimension];
         this.name = name;
     }
-    
+
     public void appendProteinCode(String proteinCode){
-        this.put(proteinCode);
-        // this.proteinCode.append(proteinCode);
+        this.proteinCode.append(proteinCode);
     }
-    
+
     public String getName(){
         return name;
     }
-    
+
     public int[] getValues(){
         return values;
     }
-    
+
     public void put(String key){
         int keyHashCode = hashCode(key);
-        
+
         while(keys[keyHashCode] != null){
             if(key.equals(keys[keyHashCode])){
                 values[keyHashCode]++;
@@ -43,14 +42,14 @@ public class Gorilla{
         }
         if(keys[keyHashCode] == null){
             keys[keyHashCode] = key;
-            values[keyHashCode] = 1;  
+            values[keyHashCode] = 1;
         }
     }
-    
+
     public int hashCode(String s){
         return (s.hashCode() & 0x7fffffff) % dimension;
     }
-    
+
     public void addSubstrings(){
         int counter = 0;
         String proteinCode = this.proteinCode.toString();
@@ -61,7 +60,7 @@ public class Gorilla{
             if (counter % 100 == 0) StdOut.println("... substrings: "+counter);
         }
     }
-    
+
     public static double vectorLength(int[] coordinates){
         int sum = 0;
         for(int i : coordinates){
@@ -69,31 +68,31 @@ public class Gorilla{
         }
         return Math.sqrt(sum);
     }
-    
+
     public static double vectorAngle(int[] a, int[] b){
         double angle;
         angle = dotProduct(a, b) / (vectorLength(a) * vectorLength(b));
         return angle;
     }
-    
+
     public static int dotProduct(int[] a, int[] b){
         int sum = 0;
-        
+
         for(int i = 0 ; i < a.length ; i++){
             sum += a[i]*b[i];
         }
-        
+
         return sum;
     }
-    
-    
+
+
     public static void main(String[] args){
         ArrayList<Gorilla> list = new ArrayList<Gorilla>();
-        
+
         String input, name = "ignore";
         int nCodes = 0;
         Gorilla gorilla = new Gorilla(name);
-        
+
         while(!StdIn.isEmpty()){
             input = StdIn.readString();
             int indexOfName = input.indexOf('>');
@@ -121,12 +120,11 @@ public class Gorilla{
             if(nCodes % 1000 == 0) StdOut.println(""+name+":"+nCodes);
         }
 
-        /*
         StdOut.println("Finding and adding substrings...");
         for(Gorilla g : list){
             g.addSubstrings();
             StdOut.println("Added substrings to: "+g.name);
-        }*/
+        }
 
         StdOut.println("Comparing gorillas...");
         for (int j = 0; j < list.size(); j++) {
