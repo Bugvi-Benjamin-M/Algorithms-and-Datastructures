9ae31dbc2f29a5d49a69b5b5300ce7958875eb0e
Niclas Hedam <niclas@frax.dk>
2017-04-26 16:55:19 +0200

    Works on *nix machines

--- SpanningUSA/SpanningUSA.java
+++ SpanningUSA/SpanningUSA.java
@@ -3,63 +3,65 @@ import java.util.regex.*;
 import java.util.*;
 
 /**
- * Spanning USA assignment. 
+ * Spanning USA assignment.
  * @author Jakob Mollerup
  * @author Benjamin Magnussen
  * @author Andreas Blanke
  * @author Niclas Hedam
- * 
+ *
  */
 
 public class SpanningUSA extends EdgeWeightedGraph {
-    
+
     private static HashMap<String, Integer> cityToIdMap = new HashMap();
     private static HashMap<Integer, String> idToCityMap = new HashMap();
     private static int counter = 0;
-    
+
     public SpanningUSA() {
         super(counter);
     }
-    
+
     public static boolean isVertex(String line) {
         return !line.contains("--");
     }
-    
+
     public static int parseVertex(String line) {
         String regex = "\"?(.+)\"?";
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(line);
-        return stringToInt(m.group(0));
+        m.find();
+        return stringToInt(m.group(1).trim());
     }
-    
+
     public static Edge parseEdge(String line) {
         String regex = "\"?(.+)\"?--\"?(.+)\"?\\[(.+)\\]";
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(line);
-        Edge edge = new Edge(stringToInt(m.group(0)), stringToInt(m.group(1)), 
-                             Integer.parseInt(m.group(2)));
+        m.find();
+        Edge edge = new Edge(stringToInt(m.group(1).trim()), stringToInt(m.group(2).trim()),
+                             Integer.parseInt(m.group(3).trim()));
         return edge;
-    } 
-    
+    }
+
     public static int stringToInt(String string) {
         if(!cityToIdMap.containsKey(string)) {
             cityToIdMap.put(string, counter);
             idToCityMap.put(counter, string);
             return counter++;
-        }      
-        return cityToIdMap.get(string);       
+        }
+        return cityToIdMap.get(string);
     }
-    
+
     public static String getCityName(int i) {
         return idToCityMap.get(i);
     }
 
-    
+
     public static void main(String[] args) {
-        
+
         Bag<Edge> edges = new Bag();
-        
-        
+
+
         while(!StdIn.isEmpty()) {
             String s = StdIn.readLine();
             if(isVertex(s)) {
